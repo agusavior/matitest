cdd0ff25249fe35688210f841bf590b8af40df1f
Agustín Javier Orlando <agusavior@gmail.com>
2020-02-03 12:55:26 -0300

    Mongoose conectado a un mongo remoto en Mongo Atlas. Agregado el .gitignore para ignorar node_modules

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 var express = require('express');
 var app = express();
 
+var mongoose = require('mongoose');
+mongoose.connect(
+    'mongodb+srv://mati-test-user0:aWCS8CGePfYkF0dgaFod@matitestcluster-nfoij.mongodb.net/test?retryWrites=true&w=majority',
+    { useNewUrlParser: true, useUnifiedTopology: true }
+).catch(error => console.log(error));
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('Conectado a MongoDB!');
+});
+
 app.get('/', function (req, res) {
   res.send('Hello World!');
+});
+
+app.get('/login', function (req, res) {
+    res.send('user: ' + req.query.user + '\npass: ' + req.query.pass);
 });
 
 app.listen(3000, function () {
